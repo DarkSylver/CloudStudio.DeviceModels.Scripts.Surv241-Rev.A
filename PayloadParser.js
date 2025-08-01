0ba60b40ed9f127585910c3643e3d08309a1ed4c
Cristian Zangaro <cristian.zangaro@cloud.studio>
2025-08-01 18:04:13 -0300

    Update PayloadParser.js
    Co-authored-by: Cristian Miguel Zangaro <cristian.zangaro@outlook.com>
    Co-authored-by: DarkSylver <darksylvergs@hotmail.com>

---
+++ PayloadParser.js
@@ -0,0 +1,57 @@
+function parseUplink(device, payload)
+{
+    var base64Image = payload.asJsonObject();
+    var endpoint = device.endpoints.byAddress("Surv001");
+    endpoint.uploadCameraSnapshot(base64Image, "png");
+
+}
+
+ 
+ 
+function buildDownlink(device, endpoint, command, payload) 
+{ 
+	// Esta función permite convertir un comando de la plataforma en un
+	// payload que pueda enviarse al dispositivo.
+	// Más información en https://wiki.cloud.studio/page/200
+
+	// Los parámetros de esta función, son:
+	// - device: objeto representando el dispositivo al cual se enviará el comando.
+	// - endpoint: objeto endpoint representando el endpoint al que se enviará el 
+	//   comando. Puede ser null si el comando se envía al dispositivo, y no a 
+	//   un endpoint individual dentro del dispositivo.
+	// - command: objeto que contiene el comando que se debe enviar. Más
+	//   información en https://wiki.cloud.studio/page/1195.
+
+	// Este ejemplo está escrito asumiendo un dispositivo que contiene un único 
+	// endpoint, de tipo appliance, que se puede encender, apagar y alternar. 
+	// Se asume que se debe enviar un solo byte en el payload, que indica el tipo 
+	// de operación.
+
+/*
+	 payload.port = 25; 	 	 // Este dispositivo recibe comandos en el puerto LoRaWAN 25 
+	 payload.buildResult = downlinkBuildResult.ok; 
+
+	 switch (command.type) { 
+	 	 case commandType.onOff: 
+	 	 	 switch (command.onOff.type) { 
+	 	 	 	 case onOffCommandType.turnOn: 
+	 	 	 	 	 payload.setAsBytes([30]); 	 	 // El comando 30 indica "encender" 
+	 	 	 	 	 break; 
+	 	 	 	 case onOffCommandType.turnOff: 
+	 	 	 	 	 payload.setAsBytes([31]); 	 	 // El comando 31 indica "apagar" 
+	 	 	 	 	 break; 
+	 	 	 	 case onOffCommandType.toggle: 
+	 	 	 	 	 payload.setAsBytes([32]); 	 	 // El comando 32 indica "alternar" 
+	 	 	 	 	 break; 
+	 	 	 	 default: 
+	 	 	 	 	 payload.buildResult = downlinkBuildResult.unsupported; 
+	 	 	 	 	 break; 
+	 	 	 } 
+	 	 	 break; 
+	 	 default: 
+	 	 	 payload.buildResult = downlinkBuildResult.unsupported; 
+	 	 	 break; 
+	 }
+*/
+
+}
